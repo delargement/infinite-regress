202ff8ee89bd2a9635a77ce530c0b273a494ec15
Spongiforma <56219994+Spongiforma@users.noreply.github.com>
2022-03-16 20:28:18 +0800

    get rid of tailwind

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <TInput type="text" placeholder="Search.."/>
+    <input type="text" placeholder="Search.."/>
     <router-link to="/graph"><button>Search</button></router-link>
   </div>
 </template>
@@ -8,11 +8,10 @@
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
-import {TInput} from "vue-tailwind/dist/components";
 
 @Component({
   components: {
-    HelloWorld, TInput
+    HelloWorld
   },
 })
 export default class Home extends Vue {}
